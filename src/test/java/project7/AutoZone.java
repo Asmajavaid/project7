cd47fa3d0d2efc4c1576340fde56ba2e5f49bb2b
asmajavaid <asmajavaid01@gmail.com>
2022-12-09 00:06:20 -0500

    project

---
+++ src/test/java/project7/AutoZone.java
@@ -0,0 +1,20 @@
+package project7;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class AutoZone {
+	public static WebDriver driver;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriverManager.chromedriver().setup();
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://AutoZone.com");
+
+	}
+
+}
